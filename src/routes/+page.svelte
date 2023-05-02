5da32fa87a83c02960edc98367f0b6fc6e9c1c2d
Randy Daniel <romeomikedelta@gmail.com>
2023-05-02 07:43:47 -0400

    Link to pepe.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
 <div class="h-screen flex items-center justify-center">
-	<h1 class="text-base font-medium">randy.digital</h1>
+	<div class="flex flex-col items-center">
+		<h1 class="text-base font-medium">randy.digital</h1>
+		<a href="/playground/pepe">
+			<img
+				class="rounded-lg w-10 h-auto grayscale mt-4"
+				src="https://assets.coingecko.com/coins/images/29850/large/pepe-token.jpeg?1682922725"
+				alt="Pepe"
+			/>
+		</a>
+	</div>
 </div>
